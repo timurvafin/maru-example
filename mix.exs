6c4c7d4fa210277861acb70b55098bee05ca533e
Timur Vafin <timur.vafin@flatstack.com>
2018-08-16 16:24:39 +0300

    Build permanent

--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule MaruExample.MixProject do
       app: :maru_example,
       version: "0.1.0",
       elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
